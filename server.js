9f08dc89687ea4d62696a33fd1029683a36dc6e4
kburnssr <50921973+kburnssr@users.noreply.github.com>
2019-12-01 19:17:11 -0800

    made a couple of minor changes

--- server.js
+++ server.js
@@ -5,23 +5,17 @@ var request = require('request');
 var PORT = process.env.PORT || 3001;
 var app = express();
 var path = require("path");
-
-
 // set the app up with bodyparser
 app.use(bodyParser());
-
 // Database configuration
 var databaseUrl = process.env.MONGODB_URI || "books_db";
 var collections = ["books"];
-
 // Hook mongojs config to db variable
 var db = mongojs(databaseUrl, collections);
-
 // Log any mongojs errors to console
 db.on("error", function (error) {
     console.log("Database Error:", error);
 });
-
 //allow the api to be accessed by other apps
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -29,31 +23,28 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
     next();
 });
-
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "./client/build")))
 };
-
 if (process.env.NODE_ENV === "production") {
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "./client/build/index.html"))
     })
 }
-
 app.get("/getBookInfo", (req, res) => {
     let book = req.query.book
-    console.log(book)
+    // console.log(book)
     request('https://www.googleapis.com/books/v1/volumes?q=' + book + "&projection=full&filter=ebooks", function (error, response, body) {
         if (error) {
             console.log(error)
             res.json({ error: true })
         } 
-        console.log("returning book information")
+        // console.log("returning book information")
             res.json(JSON.parse(body))
     });
 });
-
 app.post("/saveBook", (req, res) => {
+    console.log("SAVING THIS BOOK:", req.body.saved_book)
     var valid = true;
     if (valid) {
         let saved_book = req.body.saved_book;
@@ -64,33 +55,33 @@ app.post("/saveBook", (req, res) => {
             let authors = saved_book.authors;
             let price = saved_book.price;
             let link = saved_book.link;
-//            let id = saved_book.id
+           let id = saved_book.id
             db.collection("books").update(
-//                { id },
-                { title, img, description, authors, price, link },
-//                { upsert: true },
+               { id },
+               { $set: { title, img, description, authors, price, link, id }},
+               { upsert: true },
                 function(error,doc){
                     if(error){
-                        console.log(error)
+                        console.log("ERROR:", error)
                     }
-                    console.log(doc)
+                    console.log("SAVED BOOK:", doc)
                 }
             );
         } else {
             let title = saved_book.title;
             let img = saved_book.img;
             let description = saved_book.description;
-//            let id = saved_book.id
+           let id = saved_book.id
             let authors = saved_book.authors;
             db.collection("books").update(
-//               { id },
-                { title, img, description, authors },
-//                { upsert: true },
+              { id },
+              { $set:  { title, img, description, authors, id }},
+               { upsert: true },
                 function(error,doc){
                     if(error){
-                        console.log(error)
+                        console.log("ERROR:", error)
                     }
-                    console.log(doc)
+                    console.log("SAVED BOOK:", doc)
                 }
             );
         }
@@ -100,7 +91,6 @@ app.post("/saveBook", (req, res) => {
         })
     }
 })
-
 app.get("/getBooks", function (req, res) {
     db.collection("books").find(function (error, books) {
         if (error) {
@@ -110,10 +100,9 @@ app.get("/getBooks", function (req, res) {
         }
     })
 })
-
 app.post("/deleteBook", (req, res) => {
     let id = req.body.id
-    console.log(id)
+    // console.log(id)
     db.collection("books").remove({
         "_id": mongojs.ObjectID(id)
     }, function (error, removed) {
@@ -124,9 +113,6 @@ app.post("/deleteBook", (req, res) => {
         }
     });
 })
-
-
-
 // Listen on port 3001
 app.listen(PORT, function () {
     console.log('🌎 ==> Now listening on PORT %s! Visit http://localhost:%s in your browser!', PORT, PORT);
